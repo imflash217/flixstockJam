7170629c513c6ec1d3b93c02678aa499b704c606
vinaykumar2491 <vinaykumar2491@gmail.com>
2019-09-23 03:03:48 +0530

    raw implementation using pytorch

---
+++ src/run_torch_flixNet.py
@@ -0,0 +1,13 @@
+"""
+Raw implementation of training and validation script using pytorch for the task of flixstock Jam
+"""
+import torch
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+__author__ = "Vinay Kumar"
+__copyright__ = "@vinaykumar2491, 2019"
+__project__ = "flixstockJam"
+
+
