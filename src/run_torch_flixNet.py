0ad554181c22f0121f6e7e0516efb9ef69bacc44
vinaykumar2491 <vinaykumar2491@gmail.com>
2019-09-23 18:01:42 +0530

    pytorch: defining the macros

--- src/run_torch_flixNet.py
+++ src/run_torch_flixNet.py
@@ -1,13 +1,57 @@
 """
 Raw implementation of training and validation script using pytorch for the task of flixstock Jam
 """
+
+import os
+import gc
+import subprocess
+import time
+
 import torch
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+from modules import helpers
+from modules import data
+
 __author__ = "Vinay Kumar"
 __copyright__ = "@vinaykumar2491, 2019"
 __project__ = "flixstockJam"
+
+## Initial setup
+gc.collect()                                        ## garbage collection
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Running on {device}")
+
+data_preprocessing = True
+do_training = True
+do_testing = False
+root_path = f"../../data"
+data_path = f"{root_path}/images"
+attr_csv_path = f"{root_path}/attributes.csv"
+
+if data_preprocessing:
+    helpers.preprocess(data_path=data_path, attr_path=attr_csv_path, data_augmentation=False,
+                        multilabel_classification=True, target_attr="attr")
+
+if do_training:
+    ## Step-1: Preparing the training and validation dataset
+    ## Step-2: Preparing the flixNet architecture
+    ## Step-3: Designing the training and validation hyperparams, metrics etc
+    ## Step-4: Training and validating the flixNet
+    ## Step-5: Analyzing the train/valid metrics
+    ## Step-6: Saving the model, so as to be used during testing.
+    pass
+
+if do_testing:
+    ## Step-1: Prepare the test dataset
+    ## Step-2: Load the pretrained model
+    ## Step-3: Define the evaluation metrics
+    ## Step-5: Test the pretrained model against test dataset
+    ## Step-6: Record the results
+    pass
+
+
 
 
